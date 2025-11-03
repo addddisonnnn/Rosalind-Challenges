496468807c6c323492f7ae8f09dfc3e5c1b8c795
Addison Yam <99379983+addddisonnnn@users.noreply.github.com>
2025-11-03 13:43:34 -0500

    Finished Problem 6

--- code/CountingPointMutations.py
+++ code/CountingPointMutations.py
@@ -3,51 +3,29 @@ Title: Counting Point Mutations
 Link: https://rosalind.info/problems/hamm/
 
 Problem:
-The GC-content of a DNA string is given by the percentage of symbols in the string that are 'C' or 'G'. 
+Given two strings s and t of equal length, the Hamming distance between s and t, 
+denoted dH(s,t), is the number of corresponding symbols that differ in s and t.
 
-Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
-
-Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows for a default error of 0.001 in all 
-decimal answers unless otherwise stated; please see the note on absolute error below.
-
+Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
+Return: The Hamming distance dH(s,t).
 
 Sample Dataset:
-
+GAGCCTACTAACGGGAT
+CATCGTAATGACGGCCT
 
 Sample Output
-
+7
 """
 if __name__ == "__main__":
-    filename = "data/rosalind_gc.txt"
-    gc_content_dict = {}
-    current_sequence_id = None
-    current_sequence_length = 0
-    current_gc_count = 0
+    filename = "data/rosalind_hamm.txt"
+    s_seq, t_seq = "",""
 
     with open(filename, 'r') as f:
-        for line in f:
-            line = line.strip()
-            if line.startswith('>'):
-                # New sequence record
-                if current_sequence_id is not None and current_sequence_length > 0:
-                    # Calculate and store GC content for the previous sequence
-                    gc_content = (current_gc_count / current_sequence_length) * 100
-                    gc_content_dict[current_sequence_id] = gc_content
+        s_seq = f.readline().strip()
+        t_seq = f.readline().strip()
 
-                current_sequence_id = line[1:]  # Remove '>'
-                current_sequence_length = 0
-                current_gc_count = 0
-            else:
-                # Sequence line
-                current_sequence_length += len(line)
-                for char in line:
-                    if char.upper() in ('G', 'C'):
-                        current_gc_count += 1
-
-        # Process the last sequence in the file
-        if current_sequence_id is not None and current_sequence_length > 0:
-            gc_content = (current_gc_count / current_sequence_length) * 100
-            gc_content_dict[current_sequence_id] = gc_content
-    largestGCcontent = max(gc_content_dict, key=gc_content_dict.get)
-    print(largestGCcontent)
-    print(gc_content_dict[largestGCcontent])
+    distance = 0
+    for i in range(len(s_seq)):
+        if s_seq[i] != t_seq[i]:
+            distance+=1
+    print(distance)
